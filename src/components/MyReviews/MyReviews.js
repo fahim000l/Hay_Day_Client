1d32e104eaeef6c40daf7d27c7b722c5799f590d
Md Fahim Faisal <mdfahimfaisal000@gmail.com>
2022-11-09 22:22:30 +0600

    delete operation loading

--- src/components/MyReviews/MyReviews.js
+++ src/components/MyReviews/MyReviews.js
@@ -33,6 +33,13 @@ const MyReviews = () => {
                     {/* <!-- head --> */}
                     <thead>
                         <tr>
+                            <th>
+                                <label>
+                                    <button className="btn btn-square">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                    </button>
+                                </label>
+                            </th>
                             <th>Name</th>
                             <th>Text</th>
                             <th>Rating</th>
--- src/components/ServiceDetails/ReviewSection/ReviewRow/ReviewRow.js
+++ src/components/ServiceDetails/ReviewSection/ReviewRow/ReviewRow.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { FaUserAlt } from 'react-icons/fa';
+import { useLocation } from 'react-router-dom';
 
 const ReviewRow = ({ review }) => {
 
+    const location = useLocation();
 
-    const { email, image, name, rating, serviceId, text, _id } = review
+    const { email, image, name, rating, serviceId, text, _id } = review;
+
+    const handleSingleDelete = () => {
+        fetch(`http://localhost:5000/reviews/${_id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
 
     return (
         <tr>
+            <th className={`${location.pathname === '/myreviews' ? 'block' : 'hidden'}`}>
+                <label>
+                    <button onClick={handleSingleDelete} className="btn btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </button>
+                </label>
+            </th>
             <td>
                 <div className="flex items-center space-x-3">
                     <div className="avatar">
