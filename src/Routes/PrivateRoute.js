854a6ae2f7b099299fae6abc513dbaeae68cf66c
Md Fahim Faisal <mdfahimfaisal000@gmail.com>
2022-11-10 21:27:19 +0600

    loader added

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -8,7 +8,13 @@ const PrivateRoute = ({ children }) => {
     const location = useLocation();
 
     if (loader) {
-        return <p>Loading...</p>
+        return (
+            <div className="flex items-center justify-center space-x-2">
+                <div className="w-4 h-4 rounded-full animate-pulse bg-violet-400"></div>
+                <div className="w-4 h-4 rounded-full animate-pulse bg-violet-400"></div>
+                <div className="w-4 h-4 rounded-full animate-pulse bg-violet-400"></div>
+            </div>
+        );
     }
 
     if (user?.uid) {
